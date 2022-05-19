304b33721d3bb3b5feb4fa11d9d90c8e011dd5fa
Mark Sayers <marksayers46@icloud.com>
2022-05-18 19:47:55 -0500

    modifying elements

---
+++ DOM_manipulation/script.js
@@ -0,0 +1,46 @@
+// adding and creating elements by appending
+
+// const body = document.body
+// const div = document.createElement("div")
+// div.innerText = "Hello Word!"
+// body.append(div)
+
+
+
+
+// difference between innerText and textContent
+
+// const div = document.querySelector("div")
+// console.log(div.textContent)
+// console.log(div.innerText)
+
+
+
+
+//  modifying element HTML
+
+
+
+
+
+//  removing elements
+
+
+
+
+// modifying element attributes
+
+
+
+
+// modifying data attributes
+
+
+
+
+// modifying element classes
+
+
+
+
+// modifying element style
