1a1a4bf6d92bc941f16c7b8a48af3a868f8ecee6
Mark Sayers <marksayers46@icloud.com>
2022-06-06 14:19:10 -0500

    breakout blocks grid setup

--- breakout/script.js
+++ breakout/script.js
@@ -1,12 +1,45 @@
 const grid = document.querySelector('.grid')
+const blockWidth = '100px'
+const blockHeight = '20px'
 
-
-// create block function
-function addBlock() {
-    const block = document.createElement('div')
-    block.classList.add('block')
-    block.style.left = '100px'
-    block.style.bottom = '50px'
-    grid.appendChild(block)
+//create block location
+class Block {
+    constructor(xAxis, yAxis) {
+        this.bottomLeft = [xAxis, yAxis]
+        this.bottomRight = [xAxis + blockWidth, yAxis]
+        this.topLeft = [xAxis, yAxis + blockHeight]
+        this.topRight = [xAxis + blockWidth, yAxis + blockHeight]
+    }
 }
-addBlock()
+
+//all blocks location
+const blocks = [
+    new Block(10,270),
+    new Block(120,270),
+    new Block(230,270),
+    new Block(340,270),
+    new Block(450,270),
+    new Block(10,240),
+    new Block(120,240),
+    new Block(230,240),
+    new Block(340,240),
+    new Block(450,240),
+    new Block(10,210),
+    new Block(120,210),
+    new Block(230,210),
+    new Block(340,210),
+    new Block(450,210)
+]
+console.log(blocks[0])
+
+//draw all blocks
+function addBlocks() {
+    for (let i = 0; i < blocks.length; i++) {
+        const block = document.createElement('div')
+        block.classList.add('block')
+        block.style.left = blocks[i].bottomLeft[0] + 'px'
+        block.style.bottom = blocks[i].bottomLeft[1] + 'px'
+        grid.appendChild(block)
+    }
+}
+addBlocks()
